121a04d1475eaa7532b7a22d0bf6f73843a135d4
William Nguru <billnguts@gmail.com>
2013-06-06 11:42:30 +0300

    Hacked phpexcel on our CI (import files are in excel/)

--- application/controllers/c_phpexcel.php
+++ application/controllers/c_phpexcel.php
@@ -8,11 +8,81 @@ class c_phpexcel extends MY_Controller {
 function __construct()
 {
     parent::__construct();
-//  $this -> load -> model('models_sugar/M_Sugar_ExternalFort_B3');
+  $this -> load -> model('models_sugar/M_Sugar_ExternalFort_B3');
 	$this->load->library('PHPexcel'); 
 }
-	
-	function saving()
+		public function data_upload() {
+		 
+		$objPHPExcel = PHPExcel_IOFactory::load("./excel/sugar_externalfortb3.xls");	
+                    $objReader = new PHPExcel_Reader_Excel5();
+                        
+ 			$arr = $objPHPExcel -> getActiveSheet() -> toArray(null, true, true, true);
+			$highestColumm = $objPHPExcel -> setActiveSheetIndex(0) -> getHighestColumn();
+			$highestRow = $objPHPExcel -> setActiveSheetIndex(0) -> getHighestRow();
+
+			for ($i = 2; $i <= $highestRow; $i++) {
+				for ($j = 2; $j <= $highestColumm; $j++) {
+				}
+                                $inspectionRegistry = $arr[$i]['B'];
+                                $factoryName = $arr[$i]['C'];
+                                $dates = $arr[$i]['D'];
+                                $areasVisited = $arr[$i]['E'];
+                                $nonCompliances = $arr[$i]['F'];
+                                $suggestionsForImprovement = $arr[$i]['G'];
+                                $publicHealthOfficer = $arr[$i]['H'];
+                                $receivedBy = $arr[$i]['I'];
+                                $inspectorDate = $arr[$i]['J'];
+                                $receivedDate = $arr[$i]['L'];
+                                $supervisorName = $arr[$i]['N'];
+                                $supervisorDate = $arr[$i]['M'];
+/*				$this -> load -> database();
+				$query = $this -> db -> query("SELECT id FROM drugcode WHERE drug LIKE '%$commodity_name%'");
+				$results = $query -> result_array();
+				@$commodity_id = $results[0]['id'];
+				$total_issued = $arr[$i]['C'];
+				$consumption = $arr[$i]['D'];
+				$stock_on_hand = $arr[$i]['E'];
+				$earliest_expiry_date = $arr[$i]['F'];
+				$quantity_of_stock_expiring = $arr[$i]['G'];
+				$central_site_stock_on_hand = $arr[$i]['H'];
+				$total_stock_in_country = $arr[$i]['I'];
+				$mos_on_hand_pipeline = $arr[$i]['J'];
+				$mos_on_hand_central_sites = $arr[$i]['K'];
+				$mos_on_hand_total = $arr[$i]['L'];
+				$quantity_on_order_from_suppliers = $arr[$i]['M'];
+				$source = $arr[$i]['N'];
+				$expected_delivery_date = $arr[$i]['O'];
+				$receipts_or_transfers = $arr[$i]['R'];
+				$comments_or_actions = $arr[$i]['S'];
+				$upload_date = $_POST['upload_date'];
+				$pipeline_id = $_POST['pipeline_name'];
+*/
+				//pipeline id: 1= KEMSA & 2=KENYA PHARMA
+
+//				$new_pipeline = new Pipeline_Stock();
+//				$data = array("id" => 'NULL', "commodity_id" => $commodity_id, "total_issued" => $total_issued, "consumption" => $consumption, "stock_on_hand" => $stock_on_hand, "earliest_expiry_date" => $earliest_expiry_date, "quantity_of_stock_expiring" => $quantity_of_stock_expiring, "central_site_stock_on_hand" => $central_site_stock_on_hand, "total_stock_in_country" => $total_stock_in_country, "mos_on_hand_pipeline" => $mos_on_hand_pipeline, "mos_on_hand_central_sites" => $mos_on_hand_central_sites, "mos_on_hand_total" => $mos_on_hand_total, "quantity_on_order_from_suppliers" => $quantity_on_order_from_suppliers, "source" => $source, "expected_delivery_date" => $expected_delivery_date, "receipts_or_transfers" => $receipts_or_transfers, "comments_or_actions" => $comments_or_actions, "upload_date" => $upload_date, "pipeline_id" => $pipeline_id);
+//				$new_pipeline -> fromArray($data);
+//				$new_pipeline -> save();
+                                $data = array('inspectionRegistry'=>$inspectionRegistry,'factoryName'=>$factoryName,'dates'=>$dates,'areasVisited'=>$areasVisited,'nonCompliances'=>$nonCompliances,'suggestionsForImprovement'=>$suggestionsForImprovement,'publicHealthOfficer'=>$publicHealthOfficer,'receivedBy'=>$receivedBy,'inspectorDate'=>$inspectorDate,'receivedDate'=>$receivedDate,'supervisorName'=>$supervisorName,'supervisorDate'=>$supervisorDate);
+//                                echo '<pre>';
+//                                json_encode($data);
+//                                var_dump($data);
+//                                $this->M_Sugar_ExternalFort_B3->excelupload($data);
+                                  $this -> load -> database();
+                                  $this->db->query("INSERT INTO `fortification`.`sugar_externalfortb3` (`sugar_externalfortB3ID`, `inspectionRegistry`, `factoryName`, `dates`, `areasVisited`, `nonCompliances`, `suggestionsForImprovement`, `publicHealthOfficer`, `receivedBy`, `inspectorDate`, `receivedDate`, `supervisorName`, `supervisorDate`) VALUES (NULL, '$inspectionRegistry', '$factoryName', '$dates','$areasVisited', '$nonCompliances', '$suggestionsForImprovement','$publicHealthOfficer','$receivedBy', '$inspectorDate','$receivedDate','$supervisorName','$supervisorDate');");
+//                                $sql = "INSERT INTO table (title) VALUES(".$this->db->escape($title).")";
+//                                echo '</pre>';
+
+			//Pipeline_Stock::add($commodity_id,$total_issued,$consumption,$stock_on_hand,$earliest_expiry_date,$quantity_of_stock_expiring,$central_site_stock_on_hand,$total_stock_in_country,$mos_on_hand_pipeline,$mos_on_hand_central_sites,$mos_on_hand_total,$quantity_on_order_from_suppliers,$source,$expected_delivery_date,$receipts_or_transfers,$comments_or_actions,$upload_date,$pipeline_id);
+                         }
+                         echo "data saved! Thanks";
+
+		//	$this -> session -> set_userdata('upload_counter', '2');
+		//	redirect("pipeline_management/index");
+		 
+	}
+
+        function saving()
 	{
 		$PHPexcelinst = new PHPexcel;
 		$PHPexcelinst->getProperties()->setTitle("experimental")
@@ -33,46 +103,49 @@ header('Cache-Control: max-age=0'); //no cache
 	//	$objWriter->save('http://localhost/moh_fortification/excel/success.xlsx');
 	 
 	}
-	
-		function upload(){
-
-//			$location = base_url();
-//			$location = $location.'excel/big.xlsx';
-//			redirect($location); 
-//			$objReader = new PHPExcel_Reader_Excel2007();				
- 			//$objPHPExcel = $objReader -> load($location);
-			
-			$objReader = new PHPExcel_Reader_Excel2007();
-			echo $_FILES['file']['temp_name'];
-			if  ($_FILES['file']['tmp_name']) {
-				$objPHPExcel = $objReader -> load($_FILES['file']['tmp_name']);
-
-			}			
-
-	}
-
-
-	public function data_upload() {
-//		echo "<pre>";
-//		var_dump($_POST);
-//		echo $_FILES['incoming_file']['name'];
-//		echo "</pre>";
- if ($_POST['btn']=='Save') {
-
-			$objReader = new PHPExcel_Reader_Excel2007();
-
-			if ($_FILES['incoming_file']['name']) {
-				$objPHPExcel = $objReader -> load($_FILES['incoming_file']['name']);
-
-			} else {
- 
-			}
-
 	 
-		}
- // end here
-	}
+		public function houseleveluploads() {
+		 
+		$objPHPExcel = PHPExcel_IOFactory::load("./excel/IODATESURVEY-2010-donblank.xls");	
+                    $objReader = new PHPExcel_Reader_Excel5();
+                        
+ 			$arr = $objPHPExcel -> getActiveSheet() -> toArray(null, true, true, true);
+			$highestColumm = $objPHPExcel -> setActiveSheetIndex(0) -> getHighestColumn();
+			$highestRow = $objPHPExcel -> setActiveSheetIndex(0) -> getHighestRow();
 
+			for ($i = 5; $i <= $highestRow; $i++) {
+				for ($j = 2; $j <= $highestColumm; $j++) {
+				}
+                                $serial = $arr[$i]['B'];
+                                $province = $arr[$i]['C'];
+                                $district = $arr[$i]['D'];
+                                $code = $arr[$i]['E'];
+                                $school = $arr[$i]['F'];
+                                $sample = $arr[$i]['G'];
+                                $weight = $arr[$i]['H'];
+                                $brand = $arr[$i]['I'];
+                                $iodate = $arr[$i]['J'];
+//								$ndata =  array($serial,$province,$district,$code,$school,$sample,$weight,$brand,$iodate);
+                                $data = array('serial'=>$serial,'province'=>$province,'district'=>$district,'code'=>$code,'school'=>$school,'sample'=>$sample,'weight'=>$weight,'brand'=>$brand,'iodate'=>$iodate);
+                                echo '<pre>';
+//                                json_encode($data);
+//                                var_dump($data);
+//								var_dump($ndata);
+								echo "</pre>";
+//                                $this->M_Sugar_ExternalFort_B3->excelupload($data);
+                                $this -> load -> database();
+							    $this->db->query("INSERT INTO `fortification`.`houselevelresults` (`id`, `serial`, `province`, `district`, `code`, `school`, `sample`, `weight`, `brand`, `iodate`) VALUES (NULL, '$serial', '$province','$district', '$code','$school', '$sample', '$weight','$brand','$iodate');");
+//                                echo '</pre>';
+
+			//Pipeline_Stock::add($commodity_id,$total_issued,$consumption,$stock_on_hand,$earliest_expiry_date,$quantity_of_stock_expiring,$central_site_stock_on_hand,$total_stock_in_country,$mos_on_hand_pipeline,$mos_on_hand_central_sites,$mos_on_hand_total,$quantity_on_order_from_suppliers,$source,$expected_delivery_date,$receipts_or_transfers,$comments_or_actions,$upload_date,$pipeline_id);
+                         }
+                         echo "data saved! Thanks";
+
+		//	$this -> session -> set_userdata('upload_counter', '2');
+		//	redirect("pipeline_management/index");
+		 
+	}
+	
 		function index(){
 			$this->load->view('phpexcelview');
 		}
--- application/models/models_sugar/m_sugar_externalFort_B3.php
+++ application/models/models_sugar/m_sugar_externalFort_B3.php
@@ -16,51 +16,35 @@ class M_Sugar_ExternalFort_B3  extends MY_Model {
 	function addRecord($iodizationCentre) {
         $s=microtime(true); /*mark the timestamp at the beginning of the transaction*/
 		
-		if ($this -> input -> post()) {//check if a post was made
-		
+		if ($this -> input -> post()) {//check if a post was made		
 			$this->elements = array();
 			$this->theIds=array();
 			foreach ($this -> input -> post() as $key => $val) {//For every posted values
-		   // print(($key." ".$val)).'<br \>';
-			   
+		   // print(($key." ".$val)).'<br \>';			   
 			//check if posted value is among the cloned ones   
 			/* if(!strpos("_",$key)){//useful to keep all the  non-cloned elements in the loop
 			 	$key=$key."_1";
 			 }*/
-			  		//we separate the attribute name from the number
-					
-					/* $this->frags = explode("_", $key);
-				   
-				    $this->id = $this->frags[1];  // the id
-				    
-				  
-				   $this->attr = $this->frags[0];//the attribute name
-				   
+			  		//we separate the attribute name from the number					
+					/* $this->frags = explode("_", $key);				   
+				    $this->id = $this->frags[1];  // the id				  
+				   $this->attr = $this->frags[0];//the attribute name				   
 				  $this->theIds[$this->attr]=$this->id;*/
-			    //print($this->attr."  ".$this->id."  ".$val).'<br />';
-				   
+			    //print($this->attr."  ".$this->id."  ".$val).'<br />';				   
 			     $this->attr = $key;//the attribute name
 				   if (!empty($val)) 
 					//We then store the value of this attribute for this element.
 					// $this->elements[$this->id][$this->attr]=htmlentities($val);
 					$this->elements[$this->attr]=htmlentities($val);
-					
 			} //close foreach($_POST)
-			
 			//exit;
-			
 			//get the highest value of the array that will control the number of inserts to be done
 			//$this->noOfInsertsBatch=max($this->theIds);
 			$this->noOfInsertsBatch=1;
-			
-			//iodization centre name obtained from the session variable => 'affiliation'
-			
-		
+			//iodization centre name obtained from the session variable => 'affiliation'		
 			 for($i=1; $i<=$this->noOfInsertsBatch;++$i){
-			 	
-			 $this -> theForm = new \models\Entities\entities_sugar\E_Sugar_ExternalFort_B3(); //create an object of the model
-		      
-			  $this -> theForm -> setInspectionRegistry($this->elements['inspectionRegistry']);
+			  $this -> theForm = new \models\Entities\entities_sugar\E_Sugar_ExternalFort_B3(); //create an object of the model
+              $this -> theForm -> setInspectionRegistry($this->elements['inspectionRegistry']);
 			  $this -> theForm -> setDates($this->elements['inspectionDate']);
 			  $this -> theForm -> setFactoryName($iodizationCentre);
 			  $this -> theForm -> setAreasVisited($this->elements['areas_visited']);
@@ -111,20 +95,114 @@ class M_Sugar_ExternalFort_B3  extends MY_Model {
         $this->rowsInserted=$this->noOfInsertsBatch;
 		return $this -> response = 'ok';
 	}
-
+        
+		function excelupload($data){
+//			            echo '<pre>';
+//           var_dump($data);
+//            echo '</pre>';
+	$suggestionsForImprovement= $data['suggestionsForImprovement'];
+	echo $data['suggestionsForImprovement'];
+	echo "<br />";
+//foreach ($data as  $value) {
+//	echo "<pre>";
+//	echo $value['factoryName'];
+	
+//	echo "</pre>";
+//			 echo $vale[0];
+  			  $this -> theForm = new \models\Entities\entities_sugar\E_Sugar_ExternalFort_B3(); //create an object of the model
+/*				  $this -> theForm -> setInspectionRegistry($data['factoryName']);
+			  $this -> theForm -> setFactoryName($data['factoryName']);  
+		  $this -> theForm -> setDates($vale[2]);
+			  $this -> theForm -> setAreasVisited($vale[3]);
+			  $this -> theForm -> setNonCompliances($vale[4]);
+			 
+			  $this -> theForm -> setPublicHealthOfficer($vale[6]);
+ 			  $this -> theForm -> setReceivedBy($vale[7]);
+			  $this -> theForm -> setInspectorDate($vale[8]);
+			  $this -> theForm -> setReceivedDate($vale[9]);				
+			  $this -> theForm ->setSupervisorName($vale[10]);
+		  $this -> theForm ->setSupervisorDate($vale[11]);
+	*/		  
+	 $this -> theForm -> setSuggestionsForImprovement($suggestionsForImprovement);
+			  $this -> em -> persist($this -> theForm);
+			  
+//			}//end foreach
+	
+		}
+		
+        function exceluploads($exceldata){
+            echo '<pre>';
+            var_dump($exceldata);
+            echo '</pre>';
+            $this->M_Sugar_ExternalFort_B3->get_table();
+//            if ($today="leo")
+                {
+                    $s=microtime(true); /*mark the timestamp at the beginning of the transaction*/
+			 	
+                   
+			 
+			foreach ( $exceldata as  $val) {
+					
+//				var_dump($val);
+//                          
+			  //close foreach($_POST)
+			//exit;
+			//get the highest value of the array that will control the number of inserts to be done
+			//$this->noOfInsertsBatch=max($this->theIds);
+			$this->noOfInsertsBatch=1;
+			//iodization centre name obtained from the session variable => 'affiliation'		
+			 for($i=1; $i<=$this->noOfInsertsBatch;++$i){
+			  $this -> theForm = new \models\Entities\entities_sugar\E_Sugar_ExternalFort_B3(); //create an object of the model
+			  $this -> theForm -> setInspectionRegistry($val['inspectionRegistry']);
+			  $this -> theForm -> setDates($val['inspectionDate']);
+//			  $this -> theForm -> setFactoryName($val['iodizationCentre']);
+			  $this -> theForm -> setAreasVisited($val['areas_visited']);
+			  $this -> theForm -> setNonCompliances($val['nonCompliances']);
+			  $this -> theForm -> setSuggestionsForImprovement($val['suggestionsForImprovement']);
+			  $this -> theForm -> setPublicHealthOfficer($val['publicHealthOfficer']);
+			  $this -> theForm -> setReceivedBy($val['receivedBy']);
+			  $this -> theForm -> setInspectorDate($val['inspectorDate']);
+			  $this -> theForm -> setReceivedDate($val['receivedDate']);				
+			  $this -> theForm ->setSupervisorName($val['supervisorName']);
+			  $this -> theForm ->setSupervisorDate($val['supervisorDate']);
+			  $this -> em -> persist($this -> theForm);
+        	//now do a batched insert, default at 5
+                	  $this->batchSize=5;
+                          if($i % $this->batchSize==0){
+                              try{
+                                  $this -> em -> flush();
+                                  $this->em->clear(); //detaches all objects from doctrine                                                                   
+                              }catch(Exception $ex){
+				    die($ex->getMessage());
+					/*display user friendly message*/
+					
+				}//end of catch
+				
+			} else if($i<$this->batchSize || $i>$this->batchSize || $i==$this->noOfInsertsBatch && 
+			$this->noOfInsertsBatch-$i<$this->batchSize){
+				 //total records less than a batch, insert all of them
+				try{
+					
+				$this -> em -> flush();
+				$this->em->clear(); //detactes all objects from doctrine
+				}catch(Exception $ex){
+					die($ex->getMessage());
+					/*display user friendly message*/					
+				}//end of catch				
+			}//end of batch condition
+				// $this->rowsInserted=$i;
+				 } //end of innner loop
+		 
+		$e=microtime(true);
+		$this->executionTime=round($e-$s,'4');
+        $this->rowsInserted=$this->noOfInsertsBatch;
+            return "issorait";
+            } 
+        }
+        
+                                }
    function get_table()
     {
-               
-     /*   $this->db->select('table.*');
-        $this->db->like('table.field',$this->input->post('sSearch'));
-        $this->db->where('table.field2',$this->input->post('field2'));
-        $this->db->limit(
-            $this->input->post('iDisplayLength'),
-            $this->input->post('iDisplayStart')
-        );
-        $query = $this->db->get('table');
-        return $query->result();
- */
            $query = $this->em->createQuery('SELECT m FROM models\Entities\entities_sugar\E_Sugar_ExternalFort_B3 m');
            $dataresult = $query->getArrayResult();
            return $dataresult;
